efbbd9c22b69fdd9120eb974b251d4aac5e6376f
kato1971 <88402289+kato1971@users.noreply.github.com>
2021-12-24 17:41:42 +0100

    Add files via upload

---
+++ function-basics.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log("Maak het deeg.");
+    console.log("Rol het deeg uit.");
+    console.log("Spreid de topping uit over het deeg.");
+    console.log("Plaats de pizza in een op 200 graden voorverwarmde oven.");
+    console.log("Bak de pizza in 20 minuten.");
+};
+// the function is not called.
+
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("Kook de rijst");
+    console.log("Spreid de rijst uit over het noriblad.");
+    console.log("Voeg de zalm toe.");
+    console.log("Rol het noriblad strak op.");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
